c5e20955b3ef86ea3aca36c9cbd2e472bc8f4bd4
vijay <vijaysekhar.vn@gmail.com>
2023-12-29 09:38:08 +0530

    Base code

--- src/main/java/com/mission42/config/PMSConfiguration.java
+++ src/main/java/com/mission42/config/PMSConfiguration.java
@@ -1,10 +1,7 @@
 package com.mission42.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
@@ -13,11 +10,5 @@ public class PMSConfiguration {
         RedisTemplate<String, String> rt = new RedisTemplate<>();
         rt.setConnectionFactory(rcf);
         return rt;
-    }
-
-    @Bean
-    RedisConnectionFactory redisConnectionFactory(){
-        JedisClientConfiguration jedisClientConfiguration = JedisClientConfiguration.builder().
-     
     }
 }
